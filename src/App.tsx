438c2a42290b4484ecc98a937bddb4ef61115174
Kukuruza <hunter3000@mail.ru>
2023-07-13 04:26:22 +1000

    Верстка страницы портфолио и создание страницы Counter

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Aside from "./components/AsideComponent";
 import PostList from "./components/PostList";
 import Post from "./pages/PostPage";
 import Portfolio from "./pages/PortfolioPage";
+import Counter from "./pages/Counter";
 
 const App: React.FC = () => {
   return (
@@ -18,6 +19,7 @@ const App: React.FC = () => {
               <Route path="/" element={<PostList />} end />
               <Route path="/post/:id" element={<Post />} />
               <Route path="/portfolio" element={<Portfolio />} />
+              <Route path="/portfolio/counter" element={<Counter />} />
             </Routes>
           </div>
         </div>
--- src/pages/PortfolioPage.tsx
+++ src/pages/PortfolioPage.tsx
@@ -1,5 +1,40 @@
 import React from "react";
 import styles from "./PortfolioPage.module.css";
+import { Link } from "react-router-dom";
+
+type Portfolios = {
+  title: string;
+  url: string;
+  text: string;
+  instruments: string;
+};
+
+const portfolios: Portfolios[] = [
+  {
+    title: "Счетчик",
+    url: "/portfolio/counter",
+    text: "Обычный счетчик (counter).",
+    instruments: "react typescript",
+  },
+  {
+    title: "Список задач",
+    url: "/",
+    text: "Список задач, в котором пользователь может добавлять, удалять и редактировать записи",
+    instruments: "vue3",
+  },
+  {
+    title: "Веб-приложение для погоды",
+    url: "/",
+    text: "Приложение, которое показывает текущую погоду в различных городах",
+    instruments: "vue3? API",
+  },
+  {
+    title: "Сайт доставки пиццы",
+    url: "/",
+    text: "Сайт пиццерии. Страница продуктов, а так же корзина",
+    instruments: "vue3 pinia",
+  },
+];
 
 const Portfolio: React.FC = () => {
   return (
@@ -7,19 +42,21 @@ const Portfolio: React.FC = () => {
       <div className="col-lg-9">
         <div className={`${styles.cardGroup} card-group`}>
           <div className="row justify-content-evenly">
-            <div className="card col">
-              <div className="card-body">
-                <h5 className="card-title">asdasdasd</h5>
-                <p className={`${styles.cardText} class-text`}>asdasdasd</p>
-                <span className="card-text">Использовал: </span>
-                <span>asdasd</span>
+            {portfolios.map((item) => (
+              <div className="card col-md-5 mb-4">
+                <div className="card-body">
+                  <h5 className="card-title">{item.title}</h5>
+                  <p className={`${styles.cardText} class-text`}>{item.text}</p>
+                  <span className="card-text">Использовал:</span>
+                  <span> {item.instruments}</span>
+                </div>
+                <div className={`${styles.cardFooter} card-footer`}>
+                  <Link className="btn btn-primary" to={item.url}>
+                    <small className="text-body-secondary">Перейти</small>
+                  </Link>
+                </div>
               </div>
-              <div className={`${styles.cardFooter} card-footer`}>
-                <a className="btn btn-primary">
-                  <small className="text-body-secondary">Перейти</small>
-                </a>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
