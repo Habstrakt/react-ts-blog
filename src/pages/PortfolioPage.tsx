b6892014489bfd7c2b7056cb828db52a9de750af
Kukuruza <hunter3000@mail.ru>
2023-07-18 01:34:17 +1000

    fix portfolio page

--- src/pages/PortfolioPage.tsx
+++ src/pages/PortfolioPage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./PortfolioPage.module.css";
 import { Link } from "react-router-dom";
+import classNames from "classnames";
 
 type Portfolios = {
   title: string;
@@ -14,25 +15,25 @@ const portfolios: Portfolios[] = [
     title: "Счетчик",
     url: "/portfolio/counter",
     text: "Обычный счетчик (counter).",
-    instruments: "react typescript",
+    instruments: "React, TypeScript",
   },
   {
     title: "Список задач",
     url: "/",
     text: "Список задач, в котором пользователь может добавлять, удалять и редактировать записи",
-    instruments: "vue3",
+    instruments: "React, TypeScript",
   },
   {
     title: "Веб-приложение для погоды",
     url: "/",
     text: "Приложение, которое показывает текущую погоду в различных городах",
-    instruments: "vue3? API",
+    instruments: "React, TypeScript, API",
   },
   {
     title: "Сайт доставки пиццы",
     url: "/",
     text: "Сайт пиццерии. Страница продуктов, а так же корзина",
-    instruments: "vue3 pinia",
+    instruments: "React, TypeScript, Redux toolkit",
   },
 ];
 
@@ -40,19 +41,21 @@ const Portfolio: React.FC = () => {
   return (
     <>
       <div className="col-lg-9">
-        <div className={`${styles.cardGroup} card-group`}>
+        <div className={classNames("styles.cardGroup", "card-group")}>
           <div className="row justify-content-evenly">
             {portfolios.map((item) => (
-              <div className="card col-md-5 mb-4">
+              <div className="card col-md-5 mb-4" key={item.title}>
                 <div className="card-body">
                   <h5 className="card-title">{item.title}</h5>
-                  <p className={`${styles.cardText} class-text`}>{item.text}</p>
+                  <p className={classNames(styles.cardText, "class-text")}>
+                    {item.text}
+                  </p>
                   <span className="card-text">Использовал:</span>
                   <span> {item.instruments}</span>
                 </div>
-                <div className={`${styles.cardFooter} card-footer`}>
+                <div className={classNames(styles.cardFooter, "card-footer")}>
                   <Link className="btn btn-primary" to={item.url}>
-                    <small className="text-body-secondary">Перейти</small>
+                    <small>Перейти</small>
                   </Link>
                 </div>
               </div>
